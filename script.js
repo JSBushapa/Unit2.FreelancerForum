974923a84960d5548df595bdafc8339ce7145d9c
jonathan bushapa <jonathanbushapa@Mac-mini-7.home>
2024-02-08 13:55:40 -0600

    added html and js contents

---
+++ script.js
@@ -0,0 +1,9 @@
+const mainT = document.querySelector("mainContent");
+const tableHead = document.createElement("th");
+tableHead.textContent = "Name";
+// mainT.append(tableHead);
+
+
+
+// random average price 
+// random freelancer list
